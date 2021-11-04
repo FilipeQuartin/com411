061e85f3f318de24ed63ccceede2d9eed1a72c08
Filipe Bastos <5bastf99@solent.ac.uk>
2021-11-04 12:56:24 +0000

    s

---
+++ data/tuples/nested_tuple.py
@@ -0,0 +1,6 @@
+def steps():
+    likelihoods = {("step 1", 50),("step 2", 38),("step 3", 27),("step 4", 99),("step 5", 4)}
+     return(likelihoods)
+def run():
+    good_steps = {}
+    bad_steps = {}
